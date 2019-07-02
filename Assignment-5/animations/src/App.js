4cb2a3b1c1187bbefcdaf91dfe2da12d0f85c482
srikanthbhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-07-02 16:32:19 +0530

    first problem of animations assignment(a-5) submitted

---
+++ Assignment-5/animations/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Animation1 from './components/Animation1'
+
+function App() {
+  return (
+      <div className="app-content">
+        <Animation1 />      
+      </div>
+  );
+}
+
+export default App;
---
+++ Assignment-5/animations/src/components/Animation1/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+function Animation1(props){
+    return (
+        <div className="text-division">Click Me</div>
+    )
+}
+
+
+export default Animation1
