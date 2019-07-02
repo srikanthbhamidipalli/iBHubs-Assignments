b0332da6b2e68ba390ba73a0379c906ad90ebdbc
srikanthbhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-07-02 17:04:02 +0530

    second animation problem added

--- Assignment-5/animations/src/App.js
+++ Assignment-5/animations/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Animation1 from './components/Animation1'
-
+import Animation2 from './components/Animation2'
 function App() {
   return (
       <div className="app-content">
-        <Animation1 />      
+        {/* <Animation1 /> */}
+        <Animation2 />    
       </div>
   );
 }
