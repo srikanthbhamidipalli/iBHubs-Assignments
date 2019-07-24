20f564982dcac08d72db77a2b7a1f0391a5dc870
srikanthbhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-07-24 10:28:31 +0530

    Memory game Assignment modified during evaluation

--- Assignment-6/gridgame/src/components/Home/GridTile/index.js
+++ Assignment-6/gridgame/src/components/Home/GridTile/index.js
@@ -1,40 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css'
+import "./styles.css";
 
-class GridTile extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            bgColor:'',
-            className:props.className,
-        }
-        setTimeout(() => {this.updateState('grid-tile');console.log("reset")},props.ms);
+class GridTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColor: "",
+      className: props.className
+    };
+    setTimeout(() => {
+      this.updateState("grid-tile");
+      console.log("reset");
+    }, props.ms);
+  }
+
+  updateState = ClassName => {
+    this.setState({
+      className: ClassName
+    });
+  };
+
+  handleClick = e => {
+    let id = parseInt(e.target.id, 10);
+    if (this.props.correct.includes(id)) {
+      this.setState({ bgColor: "green" });
+      this.props.callbackFromParent(id);
+    } else {
+      this.setState({ bgColor: "red" });
+      setTimeout(() => {
+        this.props.callbackFromParent(id);
+      }, 1000);
     }
+  };
 
-    updateState = (ClassName) => {
-        this.setState({
-            className:ClassName,
-        })
-    }
-
-    handleClick = (e) => {
-        let id=parseInt(e.target.id,10)
-        if(this.props.correct.includes(id)){
-            this.setState({bgColor:'green'})
-            this.props.callbackFromParent(id);
-        }
-        else{
-            this.setState({bgColor:'red'});
-            setTimeout(() => {this.props.callbackFromParent(id);} ,1000)
-        }
-    }
-
-    render(){
-        return(
-            <div className={this.state.className} id={this.props.value} onClick={this.handleClick} style={{backgroundColor:this.state.bgColor}}></div>
-        )
-    }
+  render() {
+    return (
+      <div
+        className={this.state.className}
+        id={this.props.value}
+        onClick={this.handleClick}
+        style={{ backgroundColor: this.state.bgColor }}
+      />
+    );
+  }
 }
 
-export default GridTile
+export default GridTile;
