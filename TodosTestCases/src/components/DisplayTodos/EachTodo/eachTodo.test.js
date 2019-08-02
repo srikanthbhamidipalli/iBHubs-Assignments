2ea87297e1123241ce6c771455a95e120a8be5da
srikanthbhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-02 15:14:27 +0530

    [red state] it should check if the todo is removing from todolist when click on cross mark

--- TodosTestCases/src/components/DisplayTodos/EachTodo/eachTodo.test.js
+++ TodosTestCases/src/components/DisplayTodos/EachTodo/eachTodo.test.js
@@ -2,6 +2,7 @@ import EachTodo from ".";
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import TodoModel from "../../../Models/TodoModel";
+import TodoStore from "../../../Stores/TodoStore";
 describe("EachTodo component test cases", () => {
   it("should update the todo isCompleted status", () => {
     const todoItem = new TodoModel("hello");
@@ -10,5 +11,14 @@ describe("EachTodo component test cases", () => {
     const inputBox = getByTestId("inputBox");
     fireEvent.click(inputBox);
     expect(todoItem.toggleCompletedStatus).toBeCalled();
+  });
+  it("should check if the todo is removing from todolist when click on cross mark", () => {
+    const todoStore = new TodoStore();
+    const todoItem = new TodoModel("hello");
+    jest.spyOn(todoStore, "removeTodo");
+    const { getByTestId } = render(<EachTodo todoItem={todoItem} />);
+    const crossmark = getByTestId("crossmark");
+    fireEvent.click(crossmark);
+    expect(todoStore.removeTodo).toBeCalled();
   });
 });
